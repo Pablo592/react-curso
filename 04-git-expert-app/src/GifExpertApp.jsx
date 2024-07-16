845608e24116a7001e593ddc6233b389d4d80019
Pablo Gaido <pablo.gaido@unc.edu.ar>
2024-07-16 15:15:26 -0300

    Ya no se guardan datos duplicados

--- 04-git-expert-app/src/GifExpertApp.jsx
+++ 04-git-expert-app/src/GifExpertApp.jsx
@@ -4,31 +4,32 @@ import { AddCategory } from './components/AddCategory'
 export const GifExpertApp = () => {
 
 
-  const [Categories, setCategories] = useState(['One Punch', 'Samurai X', 'Dragon Ball', 'Naruto'])
+  const [categories, setCategories] = useState(['One Punch', 'Samurai X', 'Dragon Ball', 'Naruto'])
 
   const onAddCategory = (newCategory) => {
-    setCategories( categories => [newCategory, ...categories] )
+    if (categories.includes(newCategory)) return
+    setCategories(categories => [newCategory, ...categories])
   }
 
   return (
     <>
-        <h1>GifExpertApp</h1>
+      <h1>GifExpertApp</h1>
 
-        <AddCategory 
-          
-            //setCategories={setCategories}
-            onNewCategory={ onAddCategory }
-        />
-        
+      <AddCategory
+
+        //setCategories={setCategories}
+        onNewCategory={onAddCategory}
+      />
 
 
-        <ol>
-          {
-            Categories.map( category => {
-              return <li key={category}>{category}</li>
-            })
-          }
-        </ol>
+
+      <ol>
+        {
+          categories.map(category => {
+            return <li key={category}>{category}</li>
+          })
+        }
+      </ol>
     </>
   )
 }
