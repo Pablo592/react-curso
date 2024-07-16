d65e25b6796915a50b6129ade2a921cac41336d5
Pablo Gaido <pablo.gaido@unc.edu.ar>
2024-07-16 15:09:02 -0300

    Los  datos se guardan en el componente padre

--- 04-git-expert-app/src/GifExpertApp.jsx
+++ 04-git-expert-app/src/GifExpertApp.jsx
@@ -6,12 +6,19 @@ export const GifExpertApp = () => {
 
   const [Categories, setCategories] = useState(['One Punch', 'Samurai X', 'Dragon Ball', 'Naruto'])
 
+  const onAddCategory = (newCategory) => {
+    setCategories( categories => [newCategory, ...categories] )
+  }
 
   return (
     <>
         <h1>GifExpertApp</h1>
 
-        <AddCategory setCategories={setCategories}/>
+        <AddCategory 
+          
+            //setCategories={setCategories}
+            onNewCategory={ onAddCategory }
+        />
         
 
 
--- 04-git-expert-app/src/components/AddCategory.jsx
+++ 04-git-expert-app/src/components/AddCategory.jsx
@@ -1,31 +1,33 @@
 import React, { useState } from 'react'
 
-export const AddCategory = ({setCategories}) => {
+export const AddCategory = ({ onNewCategory }) => {
 
     const [inputValue, setINputValue] = useState('Dragon Ball')
 
-    const onINputChange = ({target}) => {
+    const onINputChange = ({ target }) => {
         setINputValue([target.value])
     }
 
     const onSubmit = (event) => {
         event.preventDefault()
 
-         if( inputValue.toString().trim().length < 1) return 
-            setCategories(categories => [inputValue, ...categories])
-            setINputValue('')
+        if (inputValue.toString().trim().length < 1) return
+
+       // setCategories(categories => [inputValue, ...categories])
+       onNewCategory(inputValue.toString().trim())
+        setINputValue('')
     }
 
     return (
         <>
             <h2>Agregue una nueva categoria</h2>
             <form onSubmit={onSubmit}>
-                <input 
+                <input
                     type="text"
-                    name='cate' 
-                    placeholder="Buscar gifts" 
-                    value={ inputValue } 
-                    onChange={onINputChange} 
+                    name='cate'
+                    placeholder="Buscar gifts"
+                    value={inputValue}
+                    onChange={onINputChange}
                 />
             </form>
         </>
