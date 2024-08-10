9d0519956841caacfc70ca4c2eebac353ea94c0a
Pablo Gaido <pablo.gaido@unc.edu.ar>
2024-08-10 18:32:20 -0300

    Use memo

--- 05-hook-app/src/main.jsx
+++ 05-hook-app/src/main.jsx
@@ -10,9 +10,10 @@ import { MultipleCustomHooks } from './03-examples/MultipleCustomHooks.jsx'
 import { FocusScreen } from './04-useRef/FocusScreen.jsx'
 import { Layout } from './05-useLayoutEffect/Layout.jsx'
 import { Memorize } from './06-memos/Memorize.jsx'
+import { MemoHook } from './06-memos/MemoHook.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
  // <React.StrictMode>
-    <Memorize />,
+    <MemoHook />,
  // </React.StrictMode>,
 )
