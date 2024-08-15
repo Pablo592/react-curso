040600ec8b98481f31620387f2679bbef4e4bada
Pablo Gaido <pablo.gaido@unc.edu.ar>
2024-08-15 19:44:56 -0300

    Termino la seccion 9

--- 05-hook-app/src/06-memos/CallbackHooks.jsx
+++ 05-hook-app/src/06-memos/CallbackHooks.jsx
@@ -8,23 +8,23 @@ export const CallbackHooks = () => {
     const [counter, setCounter] = useState(10)
 
     const incrementFather = useCallback(
-      (aumento) => {
-        setCounter((value)=> value + aumento)
-      },
-      [],
+        (aumento) => {
+            setCounter((value) => value + aumento)
+        },
+        [],
     )
-    
+
 
     //const increment = () => {
     //    setCounter(counter + 1)
     //}
 
-  return (
-    <>
-        <h1>useCallback Hook: {counter}</h1>
-        <hr />
+    return (
+        <>
+            <h1>useCallback Hook: {counter}</h1>
+            <hr />
 
-        <ShowIncrement increment={incrementFather} />    
-    </>
-  )
+            <ShowIncrement increment={incrementFather} />
+        </>
+    )
 }
--- 05-hook-app/src/main.jsx
+++ 05-hook-app/src/main.jsx
@@ -12,9 +12,10 @@ import { Layout } from './05-useLayoutEffect/Layout.jsx'
 import { Memorize } from './06-memos/Memorize.jsx'
 import { MemoHook } from './06-memos/MemoHook.jsx'
 import { CallbackHooks } from './06-memos/CallbackHooks.jsx'
+import { Padre } from './07-tarea-memo/Padre.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
  // <React.StrictMode>
-    <CallbackHooks />,
+    <Padre />,
  // </React.StrictMode>,
 )
