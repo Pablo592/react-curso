94a45c7d15555020b36c8098d8ee88e2dc37e377
Pablo Gaido <pablo.gaido@unc.edu.ar>
2024-08-20 18:54:31 -0300

    Uso de localStorage

--- 05-hook-app/src/08-useReducer/TodoApp.jsx
+++ 05-hook-app/src/08-useReducer/TodoApp.jsx
@@ -2,26 +2,43 @@ import { useReducer } from 'react'
 import { todoReducer } from './todoReducer'
 import { TodoList } from './TodoList'
 import { TodoAdd } from './TodoAdd'
+import { useEffect } from 'react'
 
 const initialState = [
-    {
-        id: new Date().getTime(),
-        description: 'Aprender React',
-        done: false,
-    },
-    {
-        id: new Date().getTime() * 3,
-        description: 'Estudiar React',
-        done: false,
-    },
+  //  {
+  //      id: new Date().getTime(),
+  //      description: 'Aprender React',
+  //      done: false,
+  //  }
 ]
+
+const init = () => {
+        
+        return JSON.parse(localStorage.getItem('todos')) || []
+    }
 
 
 export const TodoApp = () => {
 
-    const [state, dispatch] = useReducer(todoReducer, initialState)
+    const [state, dispatch] = useReducer(todoReducer, initialState, init)
+
+
+    useEffect(() => {
+      
+        localStorage.setItem('todos', JSON.stringify(state))
+    
+
+    }, [state])
+    
 
     const handleNewTodo = (todo) => {
+
+        const action = {
+            type: 'add',
+            payload: todo
+        }
+
+        dispatch(action)
 
         console.log('handleNewTodo', todo)
   
