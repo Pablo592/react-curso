b25a2122777a2f76c5c1eeb135a108c13d82b741
Pablo Gaido <pablo.gaido@unc.edu.ar>
2024-08-22 18:15:42 -0300

    NavBar implementado

--- 05-hook-app/src/09-useContext/MainApp.jsx
+++ 05-hook-app/src/09-useContext/MainApp.jsx
@@ -1,21 +1,23 @@
-import { Navigate, Route, Routes } from "react-router-dom"
+import { Link, Navigate, Route, Routes } from "react-router-dom"
 import { AboutPage } from "./AboutPage"
 import { HomePage } from "./HomePage"
 import { LoginPage } from "./LoginPage"
+import { NavBar } from "./NavBar"
 
 export const MainApp = () => {
-  return (
-    <>
-        <h1>MainApp</h1>
-        <hr />
+    return (
+        <>
+            <h1>MainApp</h1>
+            <NavBar />
+            <hr />
 
-        <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="login" element={<LoginPage />} />
-            <Route path="about" element={<AboutPage />} />
+            <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="login" element={<LoginPage />} />
+                <Route path="about" element={<AboutPage />} />
 
-            <Route path="/*" element={ <Navigate to="/about"/>} />
-        </Routes>
-    </>
-  )
+                <Route path="/*" element={<Navigate to="/about" />} />
+            </Routes>
+        </>
+    )
 }
