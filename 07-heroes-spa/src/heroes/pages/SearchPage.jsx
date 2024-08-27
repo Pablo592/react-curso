2eb26f27704df1b1d3578ad4ba94ecc127665f35
Pablo Gaido <pablo.gaido@unc.edu.ar>
2024-08-27 17:36:55 -0300

    Creo el ruteo al login

---
+++ 07-heroes-spa/src/heroes/pages/SearchPage.jsx
@@ -0,0 +1,5 @@
+export const SearchPage = () => {
+  return (
+    <h1>search</h1>
+  )
+}
--- 07-heroes-spa/src/router/AppRouter.jsx
+++ 07-heroes-spa/src/router/AppRouter.jsx
@@ -1,17 +1,15 @@
-import { Navigate, Route, Routes } from "react-router-dom"
-import { DcPage,MarvelPage } from "../heroes"
+import { Route, Routes } from "react-router-dom"
+import { HeroesRoutes} from "../heroes"
 import { LoginPage } from "../auth/pages/LoginPage"
 import { Navbar } from "../ui/components/Navbar"
 
 export const AppRouter = () => {
     return (
         <>
-            <Navbar />
             <Routes>
-                <Route path="marvel" element={<MarvelPage/>} />
-                <Route path="dc" element={<DcPage/>} />
-                <Route path="login" element={<LoginPage/>} />
-                <Route path="/" element={<Navigate to="/marvel"/>} />
+                <Route path="login" element={<LoginPage />} />
+                <Route path="/*" element={<HeroesRoutes />} />
+
             </Routes>
         </>
     )
