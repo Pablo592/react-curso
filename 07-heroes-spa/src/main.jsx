02f2cd8fc4e46c74e0a4c91b5991821eabc8c4d2
Pablo Gaido <pablo.gaido@unc.edu.ar>
2024-08-27 16:42:30 -0300

    Empiezo un nuevo proyecto

--- 07-heroes-spa/src/main.jsx
+++ 07-heroes-spa/src/main.jsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { BrowserRouter } from 'react-router-dom'
+//import './styles.css'
+import { HeroesApp } from './HeroesApp'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <HeroesApp />
+    </BrowserRouter>
   </StrictMode>,
 )
